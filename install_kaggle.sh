65fb5349e9d7814670f87fb05cde3a26745f80b6
cx1964 <cx1964@mail.com>
2023-05-14 15:35:55 +0200

    initiele script

---
+++ install_kaggle.sh
@@ -0,0 +1,13 @@
+# file: install_kaggle.sh
+# functie: install kaggle.json file in ~/.kaggle/
+# Opmerking: Als men vanuit je kaggle account
+#            mbv generarte api een api key genereerd
+#            wordt er een kaggel.json file aangemaakt in ~/Downloads.
+#            Deze file moet geplaats worden in ~/.kaggle/ subdirectory.
+#            Dit script zorgt daarvoor
+
+# create directory
+cd ~
+mkdir .kaggle
+# copy the generated json file
+cp ~/Downloads/kaggle.json ~/.kaggle/ 
